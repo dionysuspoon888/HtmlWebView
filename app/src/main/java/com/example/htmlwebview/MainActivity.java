f95a468ddc19b203f198db89ee350ace4aaecaff
dionysuspoon888 <35687678+dionysuspoon888@users.noreply.github.com>
2018-04-27 01:22:05 +0800

    Initial commit

---
+++ app/src/main/java/com/example/htmlwebview/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.htmlwebview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = findViewById(R.id.wv_1);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/cardInfo.html");
+
+
+    }
+}
